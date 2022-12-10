861fe9cccf3cf44fe2fa16cd9f9d6935951ce674
Timur_Cherevkov <ti4erev@yandex.ru>
2022-12-11 00:12:03 +0300

    first commit

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ for event in bot.longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
         user_id = str(event.user_id)
         msg = event.text.lower()
-        send_buttons(user_id, "Принял, действую")
+        send_buttons(user_id, "Принял, действую.")
         tuple_users = select_users()
         list_users = []
         for i in tuple_users:
@@ -21,17 +21,17 @@ for event in bot.longpoll.listen():
             if msg == 'начать поиск':
                 bot.send_msg(user_id, f'Привет, {bot.find_name(user_id)}')
                 bot.find_people(user_id)
-                bot.send_msg(event.user_id, 'Вот что я нашел, если не подходит жми "Еще"')
+                bot.send_msg(event.user_id, 'Вот что я нашел, если не подходит жми "Еще".')
                 bot.unseen_people(user_id, offset)
 
             elif msg == 'еще' or msg == 'ещё':
-                bot.send_msg(event.user_id, 'Я еще никого не искал. Жми "Начать поиск"')
+                bot.send_msg(event.user_id, 'Я еще никого не искал. Жми "Начать поиск".')
 
             else:
-                bot.send_msg(event.user_id, 'Я тебя не понял. Вот что я умею:'
-                                            'Начать поиск - Ищю подходящих людей, показываю первого.'
-                                            'Еще - Показываю следующего.'
-                                            'Обнулить - Удаляю базу просмотренных людей, что бы снова их показать')
+                bot.send_msg(event.user_id, 'Я тебя не понял. Вот что я умею: \n'
+                                            '"Начать поиск" - Ищю подходящих людей, показываю первого. \n'
+                                            '"Еще" - Показываю следующего. \n'
+                                            '"Обнулить" - Удаляю базу просмотренных людей, что бы снова их показать.')
 
         else:
             if msg == 'начать поиск':
@@ -39,7 +39,7 @@ for event in bot.longpoll.listen():
                 drop_people(user_id)
                 create_table_people(user_id)
                 bot.find_people(user_id)  # contains the function insert_people()
-                bot.send_msg(event.user_id, 'Вот что я нашел, если не подходит жми "Еще"')
+                bot.send_msg(event.user_id, 'Вот что я нашел, если не подходит жми "Еще".')
                 bot.unseen_people(user_id, offset)
 
             elif msg == 'еще' or msg == 'ещё':
@@ -48,15 +48,15 @@ for event in bot.longpoll.listen():
                         bot.unseen_people(user_id, offset)
                         break
                 except TypeError:
-                    bot.send_msg(event.user_id, 'Что-то пошло не так, попробуй "Начать поиск"')
+                    bot.send_msg(event.user_id, 'Что-то пошло не так, попробуй "Начать поиск".')
 
             elif msg == 'обнулить':
                 drop_seen_people(user_id)
                 create_table_seen_people(user_id)
-                bot.send_msg(event.user_id, 'Иноформация о просмотренных людях удалена')
+                bot.send_msg(event.user_id, 'Иноформация о просмотренных людях удалена.')
 
             else:
-                bot.send_msg(event.user_id, 'Я тебя не понял. Вот что я умею:'
-                                            'Начать поиск - Ищю подходящих людей, показываю первого.'
-                                            'Еще - Показываю следующего.'
-                                            'Обнулить - Удаляю базу просмотренных людей, что бы снова их показать')
+                bot.send_msg(event.user_id, 'Я тебя не понял. Вот что я умею: \n '
+                                            '"Начать поиск" - Ищю подходящих людей, показываю первого. \n'
+                                            '"Еще" - Показываю следующего. \n'
+                                            '"Обнулить" - Удаляю базу просмотренных людей, что бы снова их показать.')

5fc502b124c6265598c23c077fa459c53ea10b6d
Timur_Cherevkov <ti4erev@yandex.ru>
2022-12-11 17:18:23 +0300

    first commit

--- config.py
+++ config.py
@@ -6,8 +6,6 @@ user = 'postgres'
 password = 'your_password'
 db_name = 'your_database'
 
-offset = 0
-
             # get user token url
 # https://oauth.vk.com/authorize?client_id={community_id}&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,photos,offline&response_type=token&v=5.131&state=123456
 

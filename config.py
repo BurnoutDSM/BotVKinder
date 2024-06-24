6d6899b246d88230802486f9f2536ae9a1041303
Tim4 <114944158+BurnoutDSM@users.noreply.github.com>
2024-06-24 04:13:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 user_token = ''
 comm_token = ''
 
-host = '127.0.0.1'
-user = 'postgres'
+host = pass
+user = pass
 password = 'your_password'
 db_name = 'your_database'
 
-            # get user token url
-# https://oauth.vk.com/authorize?client_id={community_id}&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,photos,offline&response_type=token&v=5.131&state=123456
 

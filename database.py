76b7d301c8e02ea32809fd6e90598441dadb0975
Tim4 <114944158+BurnoutDSM@users.noreply.github.com>
2024-09-23 10:08:44 +0300

    Update database.py

--- database.py
+++ database.py
@@ -26,7 +26,7 @@ def insert_users(user_id):
 
 def create_table_persons():
     with conn.cursor() as cursor:
-        cursor.execute(f'CREATE TABLE IF NOT EXISTS people ('
+        cursor.execute(f'CREATE TABLE IF NOT EXISTS persons ('
                        'id serial, '
                        'user_id varchar(20), '
                        'f_name varchar(40) NOT NULL, '
@@ -34,41 +34,41 @@ def create_table_persons():
                        'vk_id varchar(20) NOT NULL, '
                        'vk_link varchar(60), '
                        'PRIMARY KEY (user_id, vk_id));')
-    print('Таблица people создана')
+    print('Таблица persons создана')
 
 
-def insert_people(user_id, f_name, l_name, vk_id, vk_link):
+def insert_persons(user_id, f_name, l_name, vk_id, vk_link):
     first_name = f_name.replace("'", "")
     last_name = l_name.replace("'", "")
     with conn.cursor() as cursor:
-        cursor.execute(f"INSERT INTO people (user_id, f_name, l_name, vk_id, vk_link) "
+        cursor.execute(f"INSERT INTO persons (user_id, f_name, l_name, vk_id, vk_link) "
                        f"VALUES ('{user_id}', '{first_name}', '{last_name}', '{vk_id}', '{vk_link}');")
 
 
-def delete_from_people(user_id):
+def delete_from_persons(user_id):
     with conn.cursor() as cursor:
-        cursor.execute(f"DELETE FROM people "
+        cursor.execute(f"DELETE FROM persons "
                        f"WHERE user_id = '{user_id}'")
 
 
-def create_table_seen_people():
+def create_table_seen_persons():
     with conn.cursor() as cursor:
-        cursor.execute(f'CREATE TABLE IF NOT EXISTS seen_people ('
+        cursor.execute(f'CREATE TABLE IF NOT EXISTS seen_persons ('
                        'id serial, '
                        'user_id varchar(20), '
                        'vk_id varchar(20), '
                        'PRIMARY KEY (user_id, vk_id));')
-    print('Таблица seen_people создана')
+    print('Таблица seen_persons создана')
 
 
-def insert_seen_people(user_id, vk_id):
+def insert_seen_persons(user_id, vk_id):
     with conn.cursor() as cursor:
-        cursor.execute(f"INSERT INTO seen_people (user_id, vk_id) VALUES ('{user_id}', '{vk_id}');")
+        cursor.execute(f"INSERT INTO seen_persons (user_id, vk_id) VALUES ('{user_id}', '{vk_id}');")
 
 
-def delete_from_seen_people(user_id):
+def delete_from_seen_persons(user_id):
     with conn.cursor() as cursor:
-        cursor.execute(F"DELETE FROM seen_people "
+        cursor.execute(F"DELETE FROM seen_persons "
                        F"WHERE user_id = '{user_id}'")
 
 
@@ -81,8 +81,8 @@ def select_users():
 def select(user_id):
     with conn.cursor() as cursor:
         cursor.execute(f"SELECT u.user_id, u.f_name, u.l_name, u.vk_id, u.vk_link, su.vk_id "
-                       f"FROM people AS u "
-                       f"LEFT JOIN seen_people AS su "
+                       f"FROM persons AS u "
+                       f"LEFT JOIN seen_persons AS su "
                        f"ON u.vk_id = su.vk_id and u.user_id = su.user_id "
                        f"WHERE su.vk_id IS NULL and u.user_id = '{user_id}';")
         return cursor.fetchone()
@@ -94,24 +94,24 @@ def drop_users():
         print('Таблица users удалена')
 
 
-def drop_people():
+def drop_persons():
     with conn.cursor() as cursor:
-        cursor.execute(f'DROP TABLE IF EXISTS people CASCADE;')
+        cursor.execute(f'DROP TABLE IF EXISTS persons CASCADE;')
         print('Таблица people удалена')
 
 
-def drop_seen_people():
+def drop_seen_persons():
     with conn.cursor() as cursor:
-        cursor.execute(f'DROP TABLE IF EXISTS seen_people CASCADE;')
-        print('Таблица seen_people удалена')
+        cursor.execute(f'DROP TABLE IF EXISTS seen_persons CASCADE;')
+        print('Таблица seen_persons удалена')
 
 
 def create_db():
     drop_users()
-    drop_people()
-    drop_seen_people()
+    drop_persons()
+    drop_seen_persons()
     create_table_users()
-    create_table_people()
-    create_table_seen_people()
+    create_table_persons()
+    create_table_seen_persons()
 
 
